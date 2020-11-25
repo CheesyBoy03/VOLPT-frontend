79a0abcb36a4bbbd3e8adf5232467daaa2fc1bfd
Van Step <van.step2186@gmail.com>
2020-11-25 12:28:16 +0400

    adding closing and opening popup for adding tasks

---
+++ js/btn.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector('.popup')
+
+function close_window(){
+    popup.classList.remove('open')
+}
+
+function open_menu(){
+    popup.classList.add('open')
+}
---
+++ js/dist/btn.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var popup = document.querySelector('.popup');
+
+function close_window() {
+  popup.classList.remove('open');
+}
+
+function open_menu() {
+  popup.classList.add('open');
+}
